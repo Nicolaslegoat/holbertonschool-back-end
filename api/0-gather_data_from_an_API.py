b1fc4497838cefaf57f19bb22fcb3ae9a7e79066
Nicolaslegoat <n.mancino03@gmail.com>
2023-12-19 15:08:00 -0800

    file ok

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,70 +1,73 @@
 #!/usr/bin/python3
-"""
-Script that fetches and displays TODO list progress
-for a given employee ID using a REST API.
-"""
 
+
+""" a Python script that, using this REST API,
+for a given employee ID,
+returns information about his/her
+TODO list progress."""
 import requests
 import sys
 
 
-def todo_list(employee_id):
-    """Fetch and display TODO list progress for a given employee."""
+def to_do(employee_ID):
+    """
+    Retrieve employee information and TODO
+    list progress based on the employee ID.
 
-    base_url = 'https://jsonplaceholder.typicode.com'
+    Args:
+        employee_ID (int): The ID of the employee.
 
-    # URLs for employees and todos
-    employee_url = f"{base_url}/users/{employee_id}"
-    todos_url = f"{base_url}/todos?userId={employee_id}"
+    Returns:
+        None
 
-    # Request to get employee data
+    Prints:
+        Displays the employee's TODO list progress.
+    """
+    url = 'https://jsonplaceholder.typicode.com'
+
+    # url for employess and todos
+    employee_url = f"{url}/users/{employee_ID}"
+    todos_url = f"{url}/todos?userId={employee_ID}"
+
+    # request to get employee url
     employee_response = requests.get(employee_url)
 
-    # Verify that the request was successful
-    if employee_response.status_code != 200:
-        print(f"Error fetching employee data. Status Code: {employee_response.status_code}")
-        sys.exit(1)
-
-    # Convert employee data to JSON format
+    # converting to json format
     employee_data = employee_response.json()
 
-    # Get employee name
-    employee_name = employee_data.get('name')
+    # verifying that request is a success
+    if employee_response.status_code == 200:
+        # get the employee's name
+        employee_name = employee_data.get('name')
 
-    # Request to get TODOs data
+    # getting todos url.
     todos_response = requests.get(todos_url)
 
-    # Verify that the request was successful
-    if todos_response.status_code != 200:
-        print(f"Error fetching TODOs data. Status Code: {todos_response.status_code}")
-        sys.exit(1)
-
-    # Convert TODOs data to JSON format
+    # converting into json format
     todos_data = todos_response.json()
 
-    # Calculate total tasks and completed tasks
-    total_tasks = len(todos_data)
-    completed_tasks = sum(task['completed'] for task in todos_data)
+    # verifying if request was a success
+    if todos_response.status_code == 200:
+        # gettin total number of tasks in todos
+        total_tasks = len(todos_data)
+        # variable to be incremented if task is completed
+        completed_tasks = 0
+    # a loop that increment completed_tasks
+    for task in todos_data:
+        completed_tasks += task['completed']
 
-    # Print the progress of the employee's TODO list
-    print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, completed_tasks, total_tasks))
 
-    # Print the titles of completed tasks
     for task in todos_data:
         if task['completed']:
-            print(f"\t{task['title']}")
+            print("\t {}".format(task['title']))
 
 
-# Execute the script if it is the main entry point.
 if __name__ == "__main__":
-
-    # Check if the correct number of command-line arguments is provided
     if len(sys.argv) != 2:
-        print("Usage: python3 script_name.py <employee_id>")
+        print("error")
         sys.exit(1)
 
-    # Convert the command-line argument to an integer
     employee_id = int(sys.argv[1])
-
-    # Call the todo_list function
-    todo_list(employee_id)
+    to_do(employee_id)
