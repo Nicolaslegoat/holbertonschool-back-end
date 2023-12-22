e566d5a5a570c85e22a60442d8dd7348a08cc869
Nicolaslegoat <n.mancino03@gmail.com>
2023-12-22 13:41:42 -0800

    update file

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-
-
 """ a Python script that, using this REST API,
 for a given employee ID,
 returns information about his/her
 TODO list progress."""
+
+
 import json
 import requests
 import sys
@@ -27,34 +27,24 @@ def todo_list(employee_ID):
             employee_name = user['username']
 
             todos_url = f"{url}/{employee_ID}/todos"
-            # getting todos url.
             todos_response = requests.get(todos_url)
-
-            # converting into json format
             todos_data = todos_response.json()
 
-            # verifying if request was a success
             if todos_response.status_code == 200:
                 employee_tasks = []
                 for task in todos_data:
                     employee_tasks.append({
+                        "username": employee_name,
                         "task": task['title'],
-                        "completed": task['completed'],
-                        "username": employee_name
+                        "completed": task['completed']
                     })
 
                 all_employees_data[str(employee_ID)] = employee_tasks
 
-        # appending datas into the json file
-        json_path = f"{employee_ID}.json"
+        json_path = 'todo_list.json'
         with open(json_path, 'w') as json_file:
-            json.dump({str(employee_ID): employee_tasks}, json_file)
+            json.dump(all_employees_data, json_file, indent=2)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("error")
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-    todo_list(employee_id)
+    todo_list()
